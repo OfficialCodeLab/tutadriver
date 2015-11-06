d7d0194a551a931055790eace4c58ef695aa6dae
Carl Eiserman <carleiserman@gmail.com>
2015-11-06 11:54:19 +0200

    Re-Added old tuta login logo

--- actions/mobile/AS_Button_c5794afb2fcc4cfcbc5626ee9ed27540.js
+++ actions/mobile/AS_Button_c5794afb2fcc4cfcbc5626ee9ed27540.js
@@ -1,3 +1,4 @@
 function AS_Button_c5794afb2fcc4cfcbc5626ee9ed27540() {
+frmTripHistory.show();
 
 }
--- actions/mobile/AS_Button_f28e5c00e84b40ec9e29e140c0aa4953.js
+++ actions/mobile/AS_Button_f28e5c00e84b40ec9e29e140c0aa4953.js
@@ -1,4 +1,4 @@
 function AS_Button_f28e5c00e84b40ec9e29e140c0aa4953() {
-frmBookingsMain.show();
+frm004Home.show();
 
 }
