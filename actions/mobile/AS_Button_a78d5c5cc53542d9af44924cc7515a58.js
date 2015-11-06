0b609c8d01ec835964ff0e72be756abc93595c33
Carl Eiserman <carleiserman@gmail.com>
2015-11-06 13:43:05 +0200

    Renamed un-named flex containers in checklist form

--- actions/mobile/AS_Button_a78d5c5cc53542d9af44924cc7515a58.js
+++ actions/mobile/AS_Button_a78d5c5cc53542d9af44924cc7515a58.js
@@ -1,5 +1,5 @@
 function AS_Button_a78d5c5cc53542d9af44924cc7515a58() {
-if((frm003CheckBox.flexCheckBoxes.FlexContainer03def5989c9b34c.imgTick3.isVisible===false)){
+if((frm003CheckBox.flexCheckBoxes.flexCheckboxQuestions.imgTick3.isVisible===false)){
 frm003CheckBox["imgTick3"]["isVisible"] = true;
 }
 else {
--- actions/mobile/AS_Button_ca05084c16724fb49236874613bffe0d.js
+++ actions/mobile/AS_Button_ca05084c16724fb49236874613bffe0d.js
@@ -1,5 +1,5 @@
 function AS_Button_ca05084c16724fb49236874613bffe0d() {
-if((frm003CheckBox.flexCheckBoxes.FlexContainer03def5989c9b34c.imgTick2.isVisible===false)){
+if((frm003CheckBox.flexCheckBoxes.flexCheckboxQuestions.imgTick2.isVisible===false)){
 frm003CheckBox["imgTick2"]["isVisible"] = true;
 }
 else {
