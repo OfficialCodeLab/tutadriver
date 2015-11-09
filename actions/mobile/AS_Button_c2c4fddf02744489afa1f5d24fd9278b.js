a91293488a7cb8bdcc034c7d6aaa5b3ead81698c
Courtney Jooste <courtney@codelab.io>
2015-11-09 15:56:30 +0200

    Improved logic and reworked sign up form
    - Started moving all actions into code modules
    - Reworked sign up form to scroll to top and not move header

--- actions/mobile/AS_Button_c2c4fddf02744489afa1f5d24fd9278b.js
+++ actions/mobile/AS_Button_c2c4fddf02744489afa1f5d24fd9278b.js
@@ -1,5 +1,5 @@
 function AS_Button_c2c4fddf02744489afa1f5d24fd9278b() {
-if((frm002SignupScreen.flexSignupNext.scrollSignupBottomB.imgTick.isVisible===false)){
+if((frm002SignupScreen.scrollSignupBottomB.imgTick.isVisible===false)){
 frm002SignupScreen["imgTick"]["isVisible"] = true;
 }
 else {
--- modules/tuta.js
+++ modules/tuta.js
@@ -4,7 +4,32 @@ function init(){
   
   frm001LoginScreen.btnSignUp.onClick = function () {frm002SignupScreen.show();};
   frm001LoginScreen.btnLogin.onClick = function() {ssa.animate.move(frm001LoginScreen.flexMainButtons, 0.25, "", "-100%", null); ssa.animate.move(frm001LoginScreen.flexLoginButtons, 0.25, "0%", "0%", null);};
-  frm001LoginScreen.btnLogin2.onClick = function() {frm003CheckBox.show()};
+  frm001LoginScreen.btnLogin2.onClick = function() {frm003CheckBox.show();};
+  
+  frm002SignupScreen.btnNextSignup.onClick = function () {switchForms(1);};
+  frm002SignupScreen.btnPager2.onClick = function () {switchForms(1);};
+  frm002SignupScreen.btnPager1.onClick = function () {switchForms(0);};
+  frm002SignupScreen.btnFinishSignup.onClick = function () {frm004Home.show();};
+  frm002SignupScreen.btnCheckAgree.onClick = function(){
+    if((frm002SignupScreen.imgTick.isVisible===false)){
+      frm002SignupScreen.imgTick.setVisibility(true);
+    }
+    else {
+      frm002SignupScreen.imgTick.setVisibility(false);
+    }
+  };
+}
+
+function switchForms(bool){
+  if(bool === 1){
+    ssa.animate.move(frm002SignupScreen.scrollSignupBottom, 0.25, "116", "-100%", null);
+    ssa.animate.move(frm002SignupScreen.scrollSignupBottomB, 0.25, "116", "0%", frm002SignupScreen.scrollSignupBottomB.scrollToBeginning());
+  }
+  else
+  {
+    ssa.animate.move(frm002SignupScreen.scrollSignupBottomB, 0.25, "116", "100%", null);
+    ssa.animate.move(frm002SignupScreen.scrollSignupBottom, 0.25, "116", "0%", frm002SignupScreen.scrollSignupBottom.scrollToBeginning());
+  }
   
 }
 
