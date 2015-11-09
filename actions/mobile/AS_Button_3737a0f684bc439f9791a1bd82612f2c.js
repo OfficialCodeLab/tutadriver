947b2447581365a9263d7369a1d7bf8c2f5325b3
Courtney Jooste <courtney@codelab.io>
2015-11-09 14:57:58 +0200

    Fixed checklist form
    - Changed theme to match new skins
    - Improved checkboxes
    - New popup skin
    - Improved popup logic

---
+++ actions/mobile/AS_Button_3737a0f684bc439f9791a1bd82612f2c.js
@@ -0,0 +1,10 @@
+function AS_Button_3737a0f684bc439f9791a1bd82612f2c(eventobject) {
+function ___onClick_513ba7ab2a454583a622cb060342c6b8_Callback(){
+
+}
+frm003CheckBox["flexConfirmCabNumber"].animate(
+kony.ui.createAnimation({"100":{"left":"100%","stepConfig":{"timingFunction":kony.anim.EASE}}}),
+{"delay":0,"iterationCount":1,"fillMode":kony.anim.FILL_MODE_FORWARDS,"duration":0},
+ {"animationEnd" : ___onClick_513ba7ab2a454583a622cb060342c6b8_Callback});
+
+}
---
+++ actions/mobile/AS_Button_bb9f9adc8fec46beacac230a8ed3cb6c.js
@@ -0,0 +1,10 @@
+function AS_Button_bb9f9adc8fec46beacac230a8ed3cb6c(eventobject) {
+function ___onClick_7a376d0566fe47569b82b5fecf94c150_Callback(){
+
+}
+frm003CheckBox["flexConfirmCabNumber"].animate(
+kony.ui.createAnimation({"100":{"left":"100%","stepConfig":{"timingFunction":kony.anim.EASE}}}),
+{"delay":0,"iterationCount":1,"fillMode":kony.anim.FILL_MODE_FORWARDS,"duration":0},
+ {"animationEnd" : ___onClick_7a376d0566fe47569b82b5fecf94c150_Callback});
+
+}
