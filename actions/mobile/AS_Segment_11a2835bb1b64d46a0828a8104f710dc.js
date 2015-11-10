b6ac41f5ee2bc3cf4f3ba9f744a2fee72551a929
Ryan Sloane <slothframe@gmail.com>
2015-11-10 12:28:44 +0200

    Cancel Confirm Popup Reskin

---
+++ actions/mobile/AS_Segment_11a2835bb1b64d46a0828a8104f710dc.js
@@ -0,0 +1,4 @@
+function AS_Segment_11a2835bb1b64d46a0828a8104f710dc() {
+frmBooking.show();
+
+}
---
+++ actions/mobile/AS_Segment_b719060c3be8407b91c34e5207d41421.js
@@ -0,0 +1,4 @@
+function AS_Segment_b719060c3be8407b91c34e5207d41421() {
+frmTripHistoryInfo.show();
+
+}
