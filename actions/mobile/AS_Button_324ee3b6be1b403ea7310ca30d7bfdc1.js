926ab970dd3b56c311e4661d256939a49fc12a4a
Ryan Sloane <slothframe@gmail.com>
2015-11-11 14:16:48 +0200

    Pickup Request, About Tuta, Individual Bookings Changes - resized pickup request flex containers as needed. - About Tuta page content added as rich text - Bookings elements moved into a flex scroll. - Layout changes made to bookings elements in scroll container - Font sizes lowered for better fit

---
+++ actions/mobile/AS_Button_324ee3b6be1b403ea7310ca30d7bfdc1.js
@@ -0,0 +1,4 @@
+function AS_Button_324ee3b6be1b403ea7310ca30d7bfdc1() {
+frmPickupRequest.show();
+
+}
---
+++ actions/mobile/AS_Button_85c864f7831f430fa3c0c620c54bc2dc.js
@@ -0,0 +1,4 @@
+function AS_Button_85c864f7831f430fa3c0c620c54bc2dc() {
+frmPickupRequest.show();
+
+}
