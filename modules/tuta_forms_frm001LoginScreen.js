96bd109a1bde38075548fa6cf524f0c9abced3fb
Carl Eiserman <carleiserman@gmail.com>
2015-11-16 17:51:59 +0200

    Removed comments from frm001LoginScreen.js

--- modules/tuta_forms_frm001LoginScreen.js
+++ modules/tuta_forms_frm001LoginScreen.js
@@ -13,53 +13,10 @@ tuta.forms.frm001LoginScreen = function() {
   // Initialize form events	
   tuta.forms.frm001LoginScreen.onInit = function(form) {
       
-    
-      	
-      /*
-    	this.header("btnMenu").onClick = function(button) {
-        ssa.util.alert("My Header Button","Clicked!");
-      };*/
   };  
   
   tuta.forms.frm001LoginScreen.onPreShow = function(form) {
     var self = this;
-/*
-      this.leftMenu = new ssa.controls.menu(
-          this.control("flexMain"), 
-          this.control("flexMenu"), 
-          ssa.controls.position.LEFT,
-          ssa.controls.behavior.MOVE_OVER,
-          0.25
-      );	
-    
-      this.rightMenu = new ssa.controls.menu(
-          this.control("flexMain"), 
-          this.control("flexRightMenu"), 
-          ssa.controls.position.RIGHT,
-          ssa.controls.behavior.OVERLAY,
-          0.25
-      );	
-    
-	  this.topMenu = new ssa.controls.menu(
-          this.control("flexMain"), 
-          this.control("flexTopMenu"), 
-          ssa.controls.position.TOP,
-          ssa.controls.behavior.MOVE_OVER,
-          0.25
-      );    
-    
-      // example of onClick event on example form for btnOne
-      this.control("btnOne").onClick = function(button) {
-        self.leftMenu.toggle();
-      };
-    
-      this.control("btnTwo").onClick = function(button) {
-        self.rightMenu.toggle();
-      };	
-     	
-      this.control("btn3").onClick = function(button) {
-        self.topMenu.toggle();
-      };*/
     
     this.moveLoginButtons = new tuta.controls.menu( 
       this.control("flexMainButtons"),
@@ -76,8 +33,5 @@ tuta.forms.frm001LoginScreen = function() {
   
   tuta.forms.frm001LoginScreen.onPostShow = function(form) {
     var self = this;
-    /*this.header("btnMenu").onClick =function(button) {
-     	self.topMenu.toggle();
-    };*/
   };
 };
