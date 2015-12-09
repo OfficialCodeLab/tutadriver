afa1fe0487272fabe2037404605c0129cadc06bc
Ryan Sloane <slothframe@gmail.com>
2015-12-09 13:25:09 +0200

    Checklist: Added function to begin testing of generating widgets

--- modules/tuta_forms_frm003CheckBox.js
+++ modules/tuta_forms_frm003CheckBox.js
@@ -14,7 +14,7 @@ tuta.forms.frm003CheckBox = function() {
   tuta.forms.frm003CheckBox.onInit = function(form) {
       
     
-      	
+      
       /*
     	this.header("btnMenu").onClick = function(button) {
         ssa.util.alert("My Header Button","Clicked!");
@@ -27,8 +27,8 @@ tuta.forms.frm003CheckBox = function() {
     //this.control("btnContinue").onClick = function(button){tuta.mobile.alert("TEST", "TEST");};
     //this.control("btnContinue").onClick = function(button){tuta.animate.move(frm003CheckBox.flexConfirmCabNumber, 0, "0", "0", null);};
     
-    
-    
+    generateButton();
+    this.control("btnCheckTest1").onClick = function(button){frm003CheckboxToggle(frm003CheckBox.imgTick1);};
     this.control("btnCheck1").onClick = function(button){frm003CheckboxToggle(frm003CheckBox.imgTick1);};
     this.control("btnCheck2").onClick = function(button){frm003CheckboxToggle(frm003CheckBox.imgTick2);};
     this.control("btnCheck3").onClick = function(button){frm003CheckboxToggle(frm003CheckBox.imgTick3);};
@@ -62,4 +62,22 @@ tuta.forms.frm003CheckBox = function() {
      	self.topMenu.toggle();
     };*/
   };
+  // Test generate one button and add to existing flexContainer.
+		function generateButton() {
+        
+  		var btnBasic ={id:"btnCheckTest1", isVisible:true, skin:"CopyslButtonGlossBlue043984d7811364e", 
+                 focusSkin:"CopyslButtonGlossBlue043984d7811364e", text:"BUTTON"};
+ 		var btnLayout ={width: "100%",containerWeight: 100, padding:[0,0,0,0], margin:[0,0,0,0],
+                        hExpand:true, vExpand:false, displayText:true, onClick: function(button){frm003CheckboxToggle(frm003CheckBox.imgTick1);}};
+        
+  		var btnPSP ={};
+        
+  		var btnCheckTest1 = new kony.ui.Button(btnBasic, btnLayout, btnPSP);
+        frm003CheckBox.flexCheckboxQuestions.add(btnCheckTest1);
+          
+	}
 };
+
+ 
+
+
